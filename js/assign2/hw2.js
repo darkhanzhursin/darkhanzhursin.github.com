a8269dd6ef6c3cbb2161ef967ad9060bdd57e029
Darkhan Zhursin <daratechn@gmail.com>
2024-05-06 09:24:23 -0500

    hw

---
+++ js/assign2/hw2.js
@@ -0,0 +1,12 @@
+function printNumbers(from, to) {
+  let current = from;
+  const intervalId = setInterval(() => {
+    console.log(current);
+    if (current === to) {
+      clearInterval(intervalId);
+    }
+    current++;
+  }, 1000);
+}
+
+printNumbers(1, 5);
